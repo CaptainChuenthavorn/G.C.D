f704fe3b6b510e0f6f1cd86c80fc3abb2e047242
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-17 09:42:31 +0700

    complete logic2

--- G.C.D/GCD.cpp
+++ G.C.D/GCD.cpp
@@ -1,40 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+/*int gcd(int, int);
 int main()
 {
-	/*int n1,n2,GCD;
+	int n1,n2,GCD;
 	GCD = 0;
 	scanf("%d", &n1);
 	scanf("%d", &n2);
+	GCD = gcd(n1, n2);
+	printf("GCD : %d", GCD);
+	return 0;
+}*/
+int gcd(int n1, int n2) {
+	int GCD;
+	GCD = 0;
 	for (int i = 2;i < n1 || i < n2;i++) {
 		if (n1 % i == 0 && n2 % i == 0) {
 			GCD = i;
-		}
-	}
-	printf("GCD : %d", GCD);*/
-	int input,GCD;
-	GCD = 0;
-	int* p;
-	scanf("%d", &input);
-	p = (int*)malloc(sizeof(int));
-	for (int i = 0;i < input;i++) {
-		scanf("%d", &p[i]);
-	}
-	for (int i = 2;i < *p[i]|| i <p[1] ;i++) {
-		if ( % i == 0 && n2 % i == 0) {
-			GCD = i;
+	return GCD;
+}
+/*int computeGCD(int a, int b) {
+	if (b == 0)
+		return a;
+	else
+		return computeGCD(b, a % b);
+}*/
+int main()
+{
+	int n1, n2;
+	scanf("%d", &n1);
+	scanf("%d", &n2);
+	printf("Result %d", computeGCD(n1, n2));
+	
 	return 0;
 }
-/*
-	int input;
-	int* p;
-	printf("How many numbers you want to compare ? : ");
-	scanf("%d", &input);
-	p = (int*)malloc(sizeof(int));
-	printf("What are they ? : ");
-	for (int i = 0;i < input;i++) {
-		scanf("%d",  &p[i]);
-	}
-	printf("\nMAX number is : %d\nMIN number is : %d", max(p,input), min(p, input));
-*/
