c4f20d1888b649d98d9de2c5a508f0182ffa2734
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-17 11:52:39 +0700

    complete LCM

--- G.C.D/GCD.cpp
+++ G.C.D/GCD.cpp
@@ -1,21 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-int gcd(int , int );
+int gcd(int* , int* );
+int lcm(int , int , int );
 int main() {
-	int n1, n2;
-	scanf("%d", &n1);
-	scanf("%d", &n2);
-	printf("Result %d", gcd(n1, n2));
+	int n1, n2,GCD,LCM;
+	printf("Enter 2 number : ");
+	scanf("%d %d", &n1,&n2);
+	GCD = gcd(&n1, &n2);
+	LCM = lcm(GCD, n1, n2);
+	printf("GCD : %d\n", GCD);
+	printf("LCM : %d", LCM);
 	return 0;
 }
-int gcd(int n1, int n2) {
+int gcd(int* n1, int* n2) {
 	int GCD;
 	GCD = 0;
-	for (int i = 2;i < n1 || i < n2;i++) {
-		if (n1 % i == 0 && n2 % i == 0) {
+	for (int i = 2;i < *n1 || i < *n2;i++) {
+		if (*n1 % i == 0 && *n2 % i == 0) {
 			GCD = i;
+		}
+	}
 	return GCD;
+}
+int lcm(int GCD,int n1,int n2)
+{
+	int LCM;
+		LCM = (n1 * n2) / GCD;
+		return LCM;
 }
 
 
