74d4595697cc7a553c79d30274a4bea28696aa9c
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-09-17 10:52:38 +0700

    hi

--- G.C.D/GCD.cpp
+++ G.C.D/GCD.cpp
@@ -1,6 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+int gcd(int , int );
+int main() {
+	int n1, n2;
+	scanf("%d", &n1);
+	scanf("%d", &n2);
+	printf("Result %d", gcd(n1, n2));
+	return 0;
+}
+int gcd(int n1, int n2) {
+	int GCD;
+	GCD = 0;
+	for (int i = 2;i < n1 || i < n2;i++) {
+		if (n1 % i == 0 && n2 % i == 0) {
+			GCD = i;
+	return GCD;
+}
+
+
 /*int gcd(int, int);
 int main()
 {
@@ -12,26 +30,9 @@ int main()
 	printf("GCD : %d", GCD);
 	return 0;
 }*/
-int gcd(int n1, int n2) {
-	int GCD;
-	GCD = 0;
-	for (int i = 2;i < n1 || i < n2;i++) {
-		if (n1 % i == 0 && n2 % i == 0) {
-			GCD = i;
-	return GCD;
-}
 /*int computeGCD(int a, int b) {
 	if (b == 0)
 		return a;
 	else
 		return computeGCD(b, a % b);
 }*/
-int main()
-{
-	int n1, n2;
-	scanf("%d", &n1);
-	scanf("%d", &n2);
-	printf("Result %d", computeGCD(n1, n2));
-	
-	return 0;
-}
